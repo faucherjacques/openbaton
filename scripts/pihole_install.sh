6f9dc2f483e49b24f9aa84a8b132837d7ea629a6
Jacques Faucher <faucherjacques@gmail.com>
2019-01-21 15:36:56 -0500

    Update pihole_install.sh

--- scripts/pihole_install.sh
+++ scripts/pihole_install.sh
@@ -7,9 +7,12 @@ echo "test" > /home/ubuntu/step1.txt
 wget -O - http://get.openbaton.org/public.gpg.key | apt-key add -
 sudo apt-get update && sudo apt-get upgrade -y
 #apt-get install -y git ems curl
-#sudo apt-get install screen dialog -y
 
 echo "test" > /home/ubuntu/step2.txt
+
+sudo apt-get install screen dialog -y
+
+echo "test" > /home/ubuntu/step3.txt
 
 #screen curl -sSL https://install.pi-hole.net | bash
 #sudo wget https://install.pi-hole.net -O pihole.sh
