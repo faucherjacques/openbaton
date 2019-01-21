4a69738e1cea57d6f6dfd661bbe098d4f82c2b35
Jacques Faucher <faucherjacques@gmail.com>
2019-01-21 15:24:41 -0500

    Update pihole_install.sh

--- scripts/pihole_install.sh
+++ scripts/pihole_install.sh
@@ -2,16 +2,16 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-touch step1.txt
+echo "test" > /home/ubuntu/step1.txt
 
 #wget -O - http://get.openbaton.org/public.gpg.key | apt-key add -
-sudo apt-get update && sudo apt-get upgrade -y
+#sudo apt-get update && sudo apt-get upgrade -y
 #apt-get install -y git ems curl
-sudo apt-get install screen dialog -y
+#sudo apt-get install screen dialog -y
 
-touch step2.txt
+#touch step2.txt
 
 #screen curl -sSL https://install.pi-hole.net | bash
-sudo wget https://install.pi-hole.net -O pihole.sh
-sudo chmod +x pihole.sh
-sudo ./pihole.sh
+#sudo wget https://install.pi-hole.net -O pihole.sh
+#sudo chmod +x pihole.sh
+#sudo ./pihole.sh
