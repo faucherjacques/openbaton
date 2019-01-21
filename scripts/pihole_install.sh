fdfd31ab7d01893111522dbeda3651650eff9ede
Jacques Faucher <faucherjacques@gmail.com>
2019-01-21 13:28:29 -0500

    Update pihole_install.sh

--- scripts/pihole_install.sh
+++ scripts/pihole_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y curl
+wget -O - http://get.openbaton.org/public.gpg.key | apt-key add -
+sudo apt-get update
+sudo apt-get install -y git ems curl
 curl -sSL https://install.pi-hole.net | bash
